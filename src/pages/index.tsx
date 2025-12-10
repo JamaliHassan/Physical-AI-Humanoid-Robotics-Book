4e2bc92088a803b6f75d031146e88517b3d521ea
JamaliHassan <balochhassan939@gmail.com>
2025-12-10 23:54:44 +0500

    fixed deployement issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/hackathon-humanoid-robotics-course-book/docs/intro">
+            to="/docs/intro">
             Start Reading the Book
           </Link>
         </div>
