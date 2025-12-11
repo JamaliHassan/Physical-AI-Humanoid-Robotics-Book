a80a72d97550487e10854391daea29c27d8e5085
JamaliHassan <balochhassan939@gmail.com>
2025-12-11 07:35:24 +0500

    commit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
 
   // GitHub pages deployment config.
   organizationName: 'JamaliHassan', // Usually your GitHub org/user name.
-  projectName: 'hackathon-humanoid-robotics-course-book', // Usually your repo name.
+  projectName: 'Physical-AI-Humanoid-Robotics-Book', // Usually your repo name.
   deploymentBranch: 'gh-pages', // Branch that GitHub Pages will deploy from.
 
   onBrokenLinks: 'warn',
